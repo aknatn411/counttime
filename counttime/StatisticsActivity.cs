3acca4267985d9eb61f8f35d63eabc8b3438cd86
Chris Hernandez <chris.j.hernandez@gmail.com>
2023-11-29 11:52:59 -0800

    fixed hamburger calculation and statistics layout

--- counttime/StatisticsActivity.cs
+++ counttime/StatisticsActivity.cs
@@ -126,16 +126,11 @@ namespace counttime
 
         static int CountDays(DayOfWeek day, DateTime start, DateTime end)
         {
-            TimeSpan ts = end - start;                       // Total duration
-            int count = (int)Math.Floor(ts.TotalDays / 7);   // Number of whole weeks
-            int remainder = (int)(ts.TotalDays % 7);         // Number of remaining days
-            int sinceLastDay = (int)(end.DayOfWeek - day);   // Number of days since last [day]
-            if (sinceLastDay < 0) sinceLastDay += 7;         // Adjust for negative days since last [day]
+            TimeSpan ts = end - DateTime.Now;                       // Total duration
+            int weeks = (int) Math.Floor((ts.TotalDays / 365.25) * 52);
+            
 
-            // If the days in excess of an even week are greater than or equal to the number days since the last [day], then count this one, too.
-            if (remainder >= sinceLastDay) count++;
-
-            return count;
+            return weeks;
         }
     }
 }
